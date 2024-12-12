765ae4e03c9c2a702f392899c434248345ca8083
Kristina N <87397396+kstinanguyen@users.noreply.github.com>
2024-12-12 12:10:00 -0800

    co-worked and completed wave 3

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, onCompleteTask, onRemoveTask }) => {
+  const onTaskClicked = () => {
+    onCompleteTask(id);
+  };
+
+  const onDeleteBtnClicked = () => {
+    onRemoveTask(id);
+  };
+
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={onTaskClicked}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={onDeleteBtnClicked}>x</button>
     </li>
   );
 };
@@ -24,6 +29,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onCompleteTask: PropTypes.func.isRequired,
+  onRemoveTask: PropTypes.func.isRequired,
 };
 
 export default Task;
